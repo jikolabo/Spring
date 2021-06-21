c1081c9a696aa78099f4def50a1208a6ed6a8a71
jikolabo <jilaboscientist@gmail.com>
2021-06-22 00:22:31 +0900

    add survey fu

--- MyFirstApplication/src/main/java/com/example/demo/app/survey/SurveyController.java
+++ MyFirstApplication/src/main/java/com/example/demo/app/survey/SurveyController.java
@@ -1,4 +1,4 @@
-package com.example.demo.app.inquiry;
+package com.example.demo.app.survey;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,47 +11,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/inquiry")
-public class InquiryController {
+@RequestMapping("/survey")
+public class SurveyController {
 	
 	@GetMapping("/form")
-	public String form(InquiryForm inquiryForm, Model model,
-						@ModelAttribute("complete") String complete) {
-		model.addAttribute("title", "Inquiry Form");
-		return "inquiry/form";
+	public String form(SurveyForm surveyForm, Model model, @ModelAttribute("complete") String complete) {
+		model.addAttribute("title", "SurveyForm");
+		return "survey/form";
 	}
 	
 	@PostMapping("/form")
-	public String formGoBack(InquiryForm inquiryForm, Model model) {
-		model.addAttribute("title", "Inquiry Form");
-		return "inquiry/form";
+	public String GoBackForm(SurveyForm surveyForm, Model model) {
+		model.addAttribute("title", "SurveyForm");
+		return "survey/form";
 	}
 	
 	@PostMapping("/confirm")
-	public String confirm(@Validated InquiryForm inquiryForm, BindingResult result,
-							Model model) {
+	public String comfirm(@Validated SurveyForm surveyForm, BindingResult result, Model model) {
 		if(result.hasErrors()) {
-			model.addAttribute("title", "Inquiry Form");
-			return "inquiry/form";
+			model.addAttribute("title", "Survey Form");
+			return "survey/form";
 		}
 		model.addAttribute("title", "Confirm Page");
-		return "inquiry/confirm";
+		return "survey/confirm";
 	}
 	
 	@PostMapping("/complete")
-	public String complete(@Validated InquiryForm inquiryForm, BindingResult result,
-							Model model,
+	public String complete(@Validated SurveyForm surveyForm, BindingResult result, Model model,
 							RedirectAttributes redirectAttributes) {
 		if(result.hasErrors()) {
-			model.addAttribute("title", "InquiryForm");
-			return "inquiry/form";
+			model.addAttribute("title", "SurveyForm");
+			return "survey/form";
 		}
 		redirectAttributes.addAttribute("complete", "Registered!");
-		return "redirect:/inquiry/form";
-		
+		return "redirect:/survey/form";
 	}
-	
-	
-	
-
 }
