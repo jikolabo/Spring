f2e4db6be80d1618788f6490fe9e2fec72dc0e9c
jikolabo <jilaboscientist@gmail.com>
2021-06-22 02:20:25 +0900

    add DAO

--- MyFirstApplication/src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ MyFirstApplication/src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -1,5 +1,9 @@
 package com.example.demo.app.inquiry;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,9 +14,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.demo.entity.Inquiry;
+import com.example.demo.service.InquiryService;
+
 @Controller
 @RequestMapping("/inquiry")
 public class InquiryController {
+	
+	private final InquiryService inquiryService;
+	
+	@Autowired
+	public InquiryController(InquiryService inquiryService) {
+		this.inquiryService = inquiryService;
+	}
+	
+	@GetMapping
+	public String index(Model model) {
+		List<Inquiry> list = inquiryService.getAll();
+		
+		model.addAttribute("inquiryList", list);
+		model.addAttribute("title", "InquiryIndex");
+		
+		return "inquiry/index";
+	}
 	
 	@GetMapping("/form")
 	public String form(InquiryForm inquiryForm, Model model,
@@ -46,6 +70,16 @@ public class InquiryController {
 			model.addAttribute("title", "InquiryForm");
 			return "inquiry/form";
 		}
+		
+		Inquiry inquiry = new Inquiry();
+		inquiry.setName(inquiryForm.getName());
+		inquiry.setEmail(inquiryForm.getEmail());
+		inquiry.setContents(inquiryForm.getContents());
+		inquiry.setCreated(LocalDateTime.now());
+		
+		inquiryService.save(inquiry);
+		
+		
 		redirectAttributes.addAttribute("complete", "Registered!");
 		return "redirect:/inquiry/form";
 		
